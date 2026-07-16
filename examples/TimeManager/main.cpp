6f19210d990726ca0a5781b05524b4ea0fd7e8ac
Q-honoka <ncr2419gp0006@edu.nca.ac.jp>
2026-07-16 10:11:15 +0900

    TimeManagerのサンプルコードにコメントを記入した。

--- examples/TimeManager/main.cpp
+++ examples/TimeManager/main.cpp
@@ -1,5 +1,14 @@
 ﻿#include <iostream>
+
+// TimeManager.h をインクルード
 #include "TimeManager.h"
+
+/*
+*	【サンプルコードについて】
+*	このファイルはTimeManagerの機能確認を目的として、
+*	生成AIを利用して作成したコードをベースに、
+*	内容を確認・修正して使用しています。
+*/
 
 int main()
 {
