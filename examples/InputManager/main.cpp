43f6b4d2912a36671353aa5ed0c613d28be1c01f
Q-honoka <ncr2419gp0006@edu.nca.ac.jp>
2026-07-19 13:48:54 +0900

    入力管理クラスを作成した。

---
+++ examples/InputManager/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// InputManager.h をインクルード
+#include "InputManager.h"
+
+/*
+*	【サンプルコードについて】
+*	このファイルはInputManagerの機能確認を目的として、
+*	生成AIを利用して作成したコードをベースに、
+*	内容を確認・修正して使用しています。
+*/
+
+int main()
+{
+	return 0;
+}
